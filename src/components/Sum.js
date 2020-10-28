ccf99ac5f6f6f8cbb9d7ac9f5cdb66f6e9842205
Steven Lindelauff <slindelauff@gmail.com>
2020-10-28 22:06:31 +0100

    styling sum component

--- src/components/Sum.js
+++ src/components/Sum.js
@@ -1,11 +1,11 @@
 //React utils
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 export default function Sum({ a, b }) {
   return (
-    <div className="max-w-xl mx-auto mt-6">
-      <div className="flex justify-between">
-        <span>{a}</span> + <span>{b}</span>
+    <div className="w-full md:w-1/2 lg:w-1/3 mx-auto mt-6 px-3">
+      <div className="flex justify-between text-6xl">
+        <span>{a}</span> + <span>{b}</span> = <span>?</span>
       </div>
     </div>
   );
